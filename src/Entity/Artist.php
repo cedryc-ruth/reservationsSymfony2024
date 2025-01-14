03548270a88e77ee8b24d9262b4d957fd92771da
Cédryc Ruth <ceruth@epfc.eu>
2025-01-14 21:23:18 +0100

    step 1

--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -4,20 +4,32 @@ namespace App\Entity;
 
 use App\Repository\ArtistRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ArtistRepository::class)]
 #[ORM\Table(name:"artists")]
+#[ApiResource(
+    normalizationContext: ['groups' => ['artist:read']],
+    denormalizationContext: ['groups' => ['artist:write']],
+    paginationEnabled: false,
+    security: "is_granted('ROLE_USER')", // Nécessite une authentification pour accéder à cette ressource
+    securityPostDenormalize: "is_granted('ROLE_ADMIN') or object.getId() == user.getId()" // Contrôle après la désérialisation
+)]
 class Artist
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['artist:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 60)]
+    #[Groups(['artist:read', 'artist:write'])]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 60)]
+    #[Groups(['artist:read', 'artist:write'])]
     private ?string $lastname = null;
 
     public function getId(): ?int
